aeeedf77bfcf558750ed60548b7a9d1d523b09dc
ekisu <dts.ramon@gmail.com>
2023-04-06 19:33:55 -0300

    comprimir state para diminuir tempo de carregamento

---
+++ image_builder/compress-state.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -veu
+
+IMAGES_OUTPUT="$(dirname "$0")"/../build/images
+UNCOMPRESSED_STATE_FILE="$IMAGES_OUTPUT"/debian-state-base.bin
+COMPRESSED_STATE_FILE="$UNCOMPRESSED_STATE_FILE".zst
+# This value goes from 1 to 19 (up to 22 with ultra mode), 1 meaning less compression and 19 more compression
+COMPRESSION_LEVEL=22
+
+zstd -"$COMPRESSION_LEVEL" -f --ultra "$UNCOMPRESSED_STATE_FILE" -o "$COMPRESSED_STATE_FILE"
