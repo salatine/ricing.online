7b3833776e9c0002a2828669e77d8a2148be2c75
sawurora <sawurora@gmail.com>
2023-05-07 05:52:14 -0300

    edit statusbar itself

--- src/components/StatusBarEditor.tsx
+++ src/components/StatusBarEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StatusBar, StatusBarWidget } from '../config'
+import { StatusBar, StatusBarPosition, StatusBarWidget } from '../config'
 import { makePartialUpdater } from '../utils';
 import StatusBarPreview from './StatusBarPreview';
 import WidgetEditor from './WidgetEditor';
@@ -32,7 +32,7 @@ export default function StatusBarEditor({ statusBar, onStatusBarUpdated }: Props
 
     const selectedWidgetEditor = selectedWidget !== null
         ? <WidgetEditor widget={selectedWidget} onWidgetUpdated={onWidgetUpdated}/>
-        : <></>
+        : <Editor statusBar={statusBar} updateStatusBar={updateStatusBar}/>
 
     return (
         <div>
@@ -45,4 +45,45 @@ export default function StatusBarEditor({ statusBar, onStatusBarUpdated }: Props
             {selectedWidgetEditor}
         </div>
     )
+}
+
+type EditorProps = {
+    statusBar: StatusBar
+    updateStatusBar: (newFields: Partial<StatusBar>) => void
+}
+
+function Editor({ statusBar, updateStatusBar }: EditorProps) {
+    return (
+        <div>
+            <select 
+                value={statusBar.position}
+                onChange={(e) => updateStatusBar({position: e.target.value as StatusBarPosition})}>
+                <option value="top">top</option>
+                <option value="bottom">bottom</option>
+                <option value="left">left</option>
+                <option value="right">right</option>
+            </select>
+
+            <input 
+                min="3"
+                max="36"
+                type="number"
+                value={statusBar.height}
+                onChange={(e) => updateStatusBar({height: parseInt(e.target.value)})}>
+            </input>
+
+            <input
+                min="3"
+                max="36"
+                type="number"
+                value={statusBar.borderWidth}
+                onChange={(e) => updateStatusBar({borderWidth: parseInt(e.target.value)})}>
+            </input>
+
+            <input 
+                type="color"
+                onChange={(e) => updateStatusBar({color: e.target.value})}>
+            </input>
+        </div>
+    );
 }
