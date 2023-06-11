8aa4855426380ce7a67c964cbf9af1ec32a90827
sawurora <sawurora@gmail.com>
2023-06-11 00:41:53 -0300

    set cypress video to false
    Co-authored-by: ekisu <ekisu@users.noreply.github.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,14 +13,12 @@ export default defineConfig({
       ]
     }
   ],
-  env: {
-    screenshotsFolder: './cypress/snapshots/actual',
-    trashAssetsBeforeRuns: true,
-    video: false
-  },
   e2e: {
     setupNodeEvents(on, config) {
       getCompareSnapshotsPlugin(on, config);
     },
   },
+  screenshotsFolder: './cypress/snapshots/actual',
+  trashAssetsBeforeRuns: true,
+  video: false
 });
