16b5a8e4e64fb6e26b950913bb7eb127e5e32f35
sawurora <sawurora@gmail.com>
2023-06-11 01:26:28 -0300

    fix errorThreshold argument in compareSnapshot

--- cypress/e2e/updatePreview.cy.ts
+++ cypress/e2e/updatePreview.cy.ts
@@ -2,7 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://localhost:8080/')
     cy.wait(75000) // FIXME load config
-    emulator().compareSnapshot('default-rice', 0.0)
+    emulator().compareSnapshot('default-rice', 0.1)
     cy.contains('Update preview').click()
     cy.wait(75000) // FIXME load config
     emulator().compareSnapshot('updated-rice', 0.1)
