c82f4c788b1a7096459e467824234a590bf629b0
sawurora <sawurora@gmail.com>
2023-06-11 00:45:16 -0300

    add e2e tests workflow & add cypress scripts to package.json
    Co-authored-by: ekisu <ekisu@users.noreply.github.com>

--- cypress/e2e/updatePreview.cy.ts
+++ cypress/e2e/updatePreview.cy.ts
@@ -1,10 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://localhost:8080/')
-    cy.wait(70000) // FIXME load config
+    cy.wait(75000) // FIXME load config
     emulator().compareSnapshot('default-rice', 0.0)
     cy.contains('Update preview').click()
-    cy.wait(70000) // FIXME load config
+    cy.wait(75000) // FIXME load config
     emulator().compareSnapshot('updated-rice', 0.1)
   })
   
